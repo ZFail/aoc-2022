177d1bc9935df5ab607446e0d3a274e37194c506
ZFail <zfail0@gmail.com>
2022-12-09 10:26:00 +0300

    day09 start

---
+++ day09/2.ts
@@ -0,0 +1,8 @@
+import { sum, getDemoInput, getInput } from '../utils'
+
+const solution = (input: string) => {
+  return input
+}
+
+console.log(solution(getDemoInput()))
+// console.log(solution(getInput()))
