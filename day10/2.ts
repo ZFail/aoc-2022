36ed1ddaf95c571715df587792ee7a0858e5f42b
ZFail <zfail0@gmail.com>
2022-12-11 03:26:04 +0300

    4 files added

---
+++ day10/2.ts
@@ -0,0 +1,8 @@
+import { sum, getDemoInput, getInput, range } from '../utils'
+
+const solution = (input: string) => {
+  return input
+}
+
+console.log(solution(getDemoInput()))
+// console.log(solution(getInput()))
