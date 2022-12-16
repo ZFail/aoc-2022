fd0fbb1b50dddaefe8a77a94221383cd239a2276
ZFail <zfail0@gmail.com>
2022-12-16 22:21:20 +0300

    day10 complete

--- day10/1.ts
+++ day10/1.ts
@@ -1,8 +1,28 @@
 import { sum, getDemoInput, getInput, range } from '../utils'
 
 const solution = (input: string) => {
-  return input
+  const barriers = [20, 60, 100, 140, 180, 220]
+
+  const lines = input.split('\n')
+  let x = 1
+  let currentCycle = 0
+  let sum = 0
+  for (const line of lines) {
+    const [instruction, incXStr] = line.split(' ')
+    let incX = 0
+    if (instruction === 'noop') {
+      currentCycle += 1
+    } else {
+      currentCycle += 2
+      incX = parseInt(incXStr)
+    }
+    if (barriers.length && currentCycle >= barriers[0]) {
+      sum += barriers.shift()! * x
+    }
+    x += incX
+  }
+  return sum
 }
 
 console.log(solution(getDemoInput()))
-// console.log(solution(getInput()))
+console.log(solution(getInput()))
--- day10/2.ts
+++ day10/2.ts
@@ -1,8 +1,40 @@
 import { sum, getDemoInput, getInput, range } from '../utils'
 
 const solution = (input: string) => {
-  return input
+  const lines = input.split('\n')
+
+  const popInstruction = (): [cycles: number, inc: number] => {
+    const line = lines.shift()
+    if (!line) throw new Error('ub')
+    const [instruction, incXStr] = line.split(' ')
+    if (instruction === 'noop') {
+      return [1, 0]
+    } else {
+      return [2, parseInt(incXStr)]
+    }
+  }
+
+  const rowSize = 40
+
+  let x = 1
+  let currentCycle = 0
+  let [cycles, incX] = popInstruction()
+
+  while (true) {
+    const pixelPos = currentCycle % rowSize
+    const charToDraw = Math.abs(x - pixelPos) <= 1 ? '#' : '.'
+    if (pixelPos === 0) process.stdout.write('\n')
+    process.stdout.write(charToDraw)
+    cycles -= 1
+    if (cycles === 0) {
+      x = x + incX
+      if (!lines.length) break
+      ;[cycles, incX] = popInstruction()
+    }
+    currentCycle++
+  }
+  process.stdout.write('\nend\n')
 }
 
 console.log(solution(getDemoInput()))
-// console.log(solution(getInput()))
+console.log(solution(getInput()))
