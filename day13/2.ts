89fe233695670359e9bb394fb4f1ec9e8c50eae1
ZFail <zfail0@gmail.com>
2022-12-17 04:39:15 +0300

    part 2

--- day13/2.ts
+++ day13/2.ts
@@ -1,8 +1,36 @@
-import { getDemoInput, getInput } from '../utils'
+import { getDemoInput, getInput, sum } from '../utils'
+
+type V = number | Array<V>
+
+function compareValues(a: V, b: V): number {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a - b
+  }
+  a = Array.isArray(a) ? a : [a]
+  b = Array.isArray(b) ? b : [b]
+  for (let i = 0; i < Math.min(a.length, b.length); ++i) {
+    const c = compareValues(a[i], b[i])
+    if (c !== 0) return c
+  }
+  if (a.length === b.length) return 0
+  return a.length - b.length
+}
 
 const solution = (input: string) => {
-  return input
+  const dividers = [[[2]], [[6]]]
+  const listOfPairs = [
+    ...input
+      .replaceAll('\n\n', '\n')
+      .split('\n')
+      .map(
+        (it) => it.split('\n').map((it) => JSON.parse(it) as V[]) as [V[], V[]]
+      ),
+    ...dividers,
+  ]
+
+  const sorted = listOfPairs.sort(compareValues)
+  return (sorted.indexOf(dividers[0]) + 1) * (sorted.indexOf(dividers[1]) + 1)
 }
 
 console.log(solution(getDemoInput()))
-// console.log(solution(getInput()))
+console.log(solution(getInput()))
