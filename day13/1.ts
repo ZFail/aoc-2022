a3cf5be7a0b63672cb26bba0355c316e47d2f78f
ZFail <zfail0@gmail.com>
2022-12-17 04:26:34 +0300

    part 1

--- day13/1.ts
+++ day13/1.ts
@@ -1,8 +1,37 @@
-import { getDemoInput, getInput } from '../utils'
+import { getDemoInput, getInput, sum } from '../utils'
+
+type V = number | Array<V>
+
+function compareValues(a: V, b: V): number {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a - b
+  }
+  a = Array.isArray(a) ? a : [a]
+  b = Array.isArray(b) ? b : [b]
+  for (let i = 0; i < Math.min(a.length, b.length); ++i) {
+    const c = compareValues(a[i], b[i])
+    if (c !== 0) return c
+  }
+  if (a.length === b.length) return 0
+  return a.length - b.length
+}
 
 const solution = (input: string) => {
-  return input
+  const listOfPairs = input
+    .split('\n\n')
+    .map(
+      (it) => it.split('\n').map((it) => JSON.parse(it) as V[]) as [V[], V[]]
+    )
+
+  return listOfPairs
+    .map((pair, idx) => {
+      const c = compareValues(pair[0], pair[1])
+      if (c === 0) throw new Error('ub')
+      return c
+    })
+    .map((c, idx) => (c < 0 ? idx + 1 : 0))
+    .reduce(sum, 0)
 }
 
 console.log(solution(getDemoInput()))
-// console.log(solution(getInput()))
+console.log(solution(getInput()))
