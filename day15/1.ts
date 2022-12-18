6be63ed658eaa8ecf760cdc938fda5b07f3b4724
ZFail <zfail0@gmail.com>
2022-12-18 03:24:06 +0300

    part 1

--- day15/1.ts
+++ day15/1.ts
@@ -1,8 +1,74 @@
-import { getDemoInput, getInput } from '../utils'
+import { compareNumber, getDemoInput, getInput, notNull } from '../utils'
 
-const solution = (input: string) => {
-  return input
+type Coord = [x: number, y: number]
+type Segment = [start: number, end: number]
+
+const rangeIntersects = (s: Segment, d: Segment) => {
+  if (s[0] > s[1]) throw Error('ub')
+  if (d[0] > d[1]) throw Error('ub')
+  return (s[0] >= d[0] && s[0] <= d[1]) || (s[1] >= d[0] && s[1] <= d[1])
 }
 
-console.log(solution(getDemoInput()))
-// console.log(solution(getInput()))
+const rangeOverlapses = (r1: Segment, r2: Segment) =>
+  rangeIntersects(r1, r2) || rangeIntersects(r2, r1)
+
+const manhattanDistance = (a: Coord, b: Coord) =>
+  Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1])
+
+const solution = (input: string, row: number) => {
+  const sensors = input.split('\n').map((it) => {
+    const parts = it.split(' ')
+    const coords = [parts[2], parts[3], parts[8], parts[9]].map((it) =>
+      parseInt(it.split(',').shift()!.split(':').shift()!.split('=').pop()!)
+    )
+    const sensorPos = coords.splice(0, 2) as Coord
+    const beaconPos = coords as Coord
+    return {
+      pos: sensorPos,
+      radius: manhattanDistance(sensorPos, beaconPos),
+      beacon: beaconPos,
+    }
+  })
+  const rowSegments = sensors
+    .map((sensor) => {
+      const rowDelta = Math.abs(sensor.pos[1] - row)
+      if (rowDelta > sensor.radius) return
+      const r = sensor.radius - rowDelta
+      return [sensor.pos[0] - r, sensor.pos[0] + r] as Segment
+    })
+    .filter(notNull)
+    .sort((a, b) => compareNumber(a[0], b[0]))
+
+  const notIntersectedSegments = rowSegments.reduce(
+    (notIntersectedSegments, segment) => {
+      const lastSeg = notIntersectedSegments.at(-1)
+      if (rangeOverlapses(lastSeg, segment)) {
+        lastSeg[0] = Math.min(lastSeg[0], segment[0])
+        lastSeg[1] = Math.max(lastSeg[1], segment[1])
+      } else {
+        notIntersectedSegments.push(segment)
+      }
+      return notIntersectedSegments
+    },
+    [rowSegments[0]]
+  )
+  console.log(notIntersectedSegments)
+  const beaconsOnRow = [
+    ...new Set(
+      sensors.filter((it) => it.beacon[1] === row).map((it) => it.beacon[0])
+    ),
+  ]
+  const beaconInSegment = (beacon: number, segment: Segment) =>
+    beacon >= segment[0] && beacon <= segment[1]
+  const beaconsInSegment = (segment: Segment) =>
+    beaconsOnRow.reduce((a, b) => a + (beaconInSegment(b, segment) ? 1 : 0), 0)
+  return notIntersectedSegments.reduce(
+    (a, b) => a + b[1] - b[0] + 1 - beaconsInSegment(b),
+    0
+  )
+}
+
+console.log(solution(getDemoInput(), 9))
+console.log(solution(getDemoInput(), 10))
+console.log(solution(getDemoInput(), 11))
+console.log(solution(getInput(), 2000000))
