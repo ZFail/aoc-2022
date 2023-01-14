f906e0e2318ab1c087fa410fa96b452e13c91d98
ZFail <zfail0@gmail.com>
2023-01-14 19:01:29 +0300

    part 1

---
+++ day20/1.ts
@@ -0,0 +1,64 @@
+import { parse } from 'path'
+import { getDemoInput, getInput, range, sum } from '../utils'
+
+type Cell = {
+  value: number
+  next: Cell
+  prev: Cell
+}
+
+const solution = (input: string) => {
+  const values = input
+    .split('\n')
+    .map(
+      (it) =>
+        ({ value: parseInt(it), next: undefined, prev: undefined } as Cell)
+    )
+  values.forEach((v, idx) => {
+    v.next = values[(idx + 1) % values.length]
+    v.prev = values[(idx + values.length - 1) % values.length]
+  })
+
+  function removeCell(cell: Cell) {
+    cell.prev.next = cell.next
+    cell.next.prev = cell.prev
+  }
+  function insertCellAfter(cell: Cell, after: Cell) {
+    cell.prev = after
+    cell.next = after.next
+    after.next.prev = cell
+    after.next = cell
+  }
+
+  for (let curCell of values) {
+    if (curCell.value === 0) continue
+    removeCell(curCell)
+    let moveTo = curCell
+    range(Math.abs(curCell.value)).forEach(() => {
+      if (curCell.value > 0) moveTo = moveTo.next
+      else moveTo = moveTo.prev
+    })
+
+    insertCellAfter(curCell, curCell.value > 0 ? moveTo : moveTo.prev)
+  }
+  const zeroCell = values.find((it) => it.value === 0)
+  let curCell = zeroCell
+  const results: Cell[] = []
+  range(1000).forEach((it, idx) => {
+    curCell = curCell.next
+  })
+  results.push(curCell)
+  range(1000).forEach((it, idx) => {
+    curCell = curCell.next
+  })
+  results.push(curCell)
+
+  range(1000).forEach((it, idx) => {
+    curCell = curCell.next
+  })
+  results.push(curCell)
+  return results.map((it) => it.value).reduce(sum, 0)
+}
+
+console.log(solution(getDemoInput()))
+console.log(solution(getInput()))
